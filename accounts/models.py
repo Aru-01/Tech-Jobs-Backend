79dd75ceaa7bb9bfb06477527cb87f49c1f665c0
aru-01 <arifu5758@gmail.com>
2026-05-04 23:42:04 +0600

    added Authentication flow

--- accounts/models.py
+++ accounts/models.py
@@ -1,49 +1,31 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+)
+from accounts.manager import UserManager
 
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        if password:
-            user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('role', 'admin')
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
     ROLE_CHOICES = (
-        ('job_seeker', 'Job Seeker'),
-        ('recruiter', 'Recruiter'),
-        ('admin', 'Admin'),
+        ("job_seeker", "Job Seeker"),
+        ("recruiter", "Recruiter"),
+        ("admin", "Admin"),
     )
 
     email = models.EmailField(unique=True)
     full_name = models.CharField(max_length=255)
     profile_image = models.URLField(max_length=500, blank=True, null=True)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='job_seeker')
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="job_seeker")
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['full_name']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["full_name"]
 
     def __str__(self):
         return self.email
