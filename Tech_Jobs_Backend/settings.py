5bdc3f589fbb919bbcb33f8772ec2cc474fe6fbc
aru-01 <arifu5758@gmail.com>
2026-05-08 07:05:25 +0600

    ready to redeploy project v.1.0.2

--- Tech_Jobs_Backend/settings.py
+++ Tech_Jobs_Backend/settings.py
@@ -142,6 +142,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "https://tech-jobs-aru.vercel.app",
 ]
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://tech-jobs-.*\.vercel\.app$",
+]
+
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
     "accept",
